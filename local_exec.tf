376671ace8e5a6719e09d66b203c03f84eb7e8bf
Vibha <vibha.vanya@gmail.com>
2020-02-14 16:08:56 +0530

    Update local_exec.tf
    update local_exec file

--- local_exec.tf
+++ local_exec.tf
@@ -39,7 +39,6 @@ resource "null_resource" "ansible-main" {
         echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
         export ANSIBLE_HOST_KEY_CHECKING=False;
         echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
-        ansible-playbook  --key=${var.pvt_key} -i jenkins-ci.ini ./ansible/04-Tomcat/web-playbook.yaml -u ubuntu -v
     
 EOT
 
