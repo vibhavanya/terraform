2086682f18338519f5e3a2877b28d4a17ee69cd8
Vibha <vibha.vanya@gmail.com>
2020-02-14 16:15:14 +0530

    Update variables.tf
    update variables.tf file

--- variables.tf
+++ variables.tf
@@ -1,17 +1,15 @@
-# TF-UPGRADE-TODO: Block type was not recognized, so this block and its contents were not automatically upgraded.
 variable "key_name" {
-  default = "ansible"
+  default = "oregon1"
 }
 
 variable "pvt_key" {
-  default = "/root/.ssh/ansible.pem"
+  default = "/root/.ssh/inkey.pem"
 }
 
 variable "us-east-zones" {
-  default = ["ap-south-1", "us-west-2"]
+  default = ["us-east-1a", "us-east-1b"]
 }
 
 variable "sg-id" {
-  default = "sg-063a1a9e75dd9e60f"
+  default = "sg-03ac9f8eb19fb3bd6"
 }
-
